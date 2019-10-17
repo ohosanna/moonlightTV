1d010d68f127010938161ce6db0ee6145477a70c
ohosanna <ohosanna@gmail.com>
2019-10-17 09:15:16 +0800

    基本功能实现

---
+++ src/App/videos.js
@@ -0,0 +1,105 @@
+// Videos List
+const videos = [
+  {
+      "title": "CCTV-1",
+      "source": "http://223.110.242.130:6610/gitv/live1/G_CCTV-1-HQ/1.m3u8"
+    },
+    {
+      "title": "CCTV-2",
+      "source": "http://117.156.28.119/270000001111/1110000050/index.m3u8"
+    },
+    {
+      "title": "CCTV-3",
+      "source": "http://223.110.242.130:6610/gitv/live1/G_CCTV-3-HQ/1.m3u8"
+    },
+    {
+      "title": "CCTV-4",
+      "source": "http://223.110.242.130:6610/gitv/live1/G_CCTV-4-HQ/1.m3u8"
+    },
+    {
+      "title": "CCTV-5",
+      "source": "http://223.110.242.130:6610/gitv/live1/G_CCTV-5-HQ/1.m3u8"
+    },
+    {
+      "title": "CCTV-5+",
+      "source": "http://111.40.205.87/PLTV/88888888/224/3221225689/index.m3u8"
+    },
+    {
+      "title": "CCTV-7",
+      "source": "http://111.40.205.87/PLTV/88888888/224/3221225733/index.m3u8"
+    },
+    {
+      "title": "CCTV-8",
+      "source": "http://223.110.242.130:6610/gitv/live1/G_CCTV-8-HQ/1.m3u8"
+    },
+    {
+      "title": "CCTV-9",
+      "source": "http://117.156.28.119/270000001111/1110000058/index.m3u8"
+    },
+    {
+      "title": "CCTV-10",
+      "source": "http://223.110.242.130:6610/gitv/live1/G_CCTV-10-HQ/1.m3u8"
+    },
+    {
+      "title": "CCTV-11",
+      "source": "http://223.110.242.130:6610/gitv/live1/G_CCTV-11-HQ/1.m3u8"
+    },
+    {
+      "title": "凤凰中文 HD",
+      "source": "http://223.82.250.72/live/fhchinese/1.m3u8"
+    },
+    {
+      "title": "凤凰资讯 HD",
+      "source": "http://223.82.250.72/live/fhzixun/1.m3u8"
+    },
+    {
+      "title": "凤凰香港HD",
+      "source": "http://223.82.250.72/live/fhhongkong/1.m3u8"
+    },
+    {
+      "title": "TVB资讯台",
+      "source": "http://e1.vdowowza.vip.hk1.tvb.com/tvblive/smil:mobilehd_financeintl.smil/playlist.m3u8"
+    },
+    {
+      "title": "TVB无线财经",
+      "source": "http://e1.vdowowza.vip.hk1.tvb.com/tvblive/smil:mobilehd_financeintl.smil/chunklist.m3u8"
+    },
+    {
+      "title": "香港卫视",
+      "source": "http://zhibo.hkstv.tv/livestream/zb2yhapo/playlist.m3u8"
+    },
+    {
+      "title": "广西卫视",
+      "source": "http://223.82.250.72/live/guangxistv/1.m3u8"
+    },
+    {
+      "title": "广东卫视",
+      "source": "http://111.40.205.87/PLTV/88888888/224/3221225736/index.m3u8"
+    },
+    {
+      "title": "东方卫视",
+      "source": "http://111.40.205.87/PLTV/88888888/224/3221225735/index.m3u8"
+    },
+    {
+      "title": "湖南卫视",
+      "source": "http://111.40.205.87/PLTV/88888888/224/3221225610/index.m3u8"
+    },
+    {
+      "title": "深圳卫视",
+      "source": "http://111.40.205.87/PLTV/88888888/224/3221225703/index.m3u8"
+    },
+    {
+      "title": "浙江卫视",
+      "source": "http://ott.fj.chinamobile.com/PLTV/88888888/224/3221225934/1.m3u8"
+    },
+    {
+      "title": "广东珠江",
+      "source": "http://nclive.grtn.cn/zjpd/sd/live.m3u8"
+    },
+    {
+      "title": "test",
+      "source": "http://60.199.188.61/HLS/WG_ETTV-N/01.m3u8"
+    }
+];
+
+export default videos;
